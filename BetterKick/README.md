8111adff14716c0299ed3f07ed56f9d02e40cb59
atomic <80856947+AtomicTyler1@users.noreply.github.com>
2026-04-02 21:29:40 +0100

    Add project files.

---
+++ BetterKick/README.md
@@ -0,0 +1,10 @@
+# Better Kick
+
+This mod makes it so you can help your friends **and** kick them at the same time!
+
+The kick now uses a keybind (Default is `V`).
+
+## Trolling
+
+Please don't troll public lobbies with this mod, it's meant for private lobbies with friends.
+Do be expected to be banned from the lobby if you do so, and may get a warning from the official peak server.
---
+++ BetterKick/src/BetterKick/Plugin.cs
@@ -0,0 +1,55 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using Photon.Pun;
+using UnityEngine;
+
+namespace BetterKick
+{
+    [BepInAutoPlugin]
+    public partial class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log { get; private set; } = null!;
+        public static Harmony harmony = null!;
+
+        public static ConfigEntry<KeyCode> kickKeybind;
+
+        private void Awake()
+        {
+            Log = Logger;
+            harmony = new Harmony(Id);
+            harmony.PatchAll();
+            Log.LogInfo($"Plugin {Name} is loaded!");
+
+            kickKeybind = Config.Bind("General", "Kick Keybind", KeyCode.V, "The keybind for kicking.");
+        }
+    }
+
+    [HarmonyPatch]
+    public static class BetterKickPatches 
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CharacterGrabbing), nameof(CharacterGrabbing.Update))]
+        public static void Character_Update_Postfix(CharacterGrabbing __instance)
+        {
+            if (Input.GetKeyDown(Plugin.kickKeybind.Value) && !__instance.character.data.isKicking)
+            {
+                if (__instance.character.photonView.IsMine)
+                {
+                    __instance.character.photonView.RPC("RPCA_Kick", RpcTarget.All);
+                }
+            }
+
+            if (!__instance.isKickMode && __instance.character.data.isKicking)
+            {
+                __instance._kickTime += Time.deltaTime;
+                if (__instance._kickTime > 1f)
+                {
+                    __instance.character.data.isKicking = false;
+                    __instance._kickTime = 0f;
+                }
+            }
+        }
+    }
+}
